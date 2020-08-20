2c916f108ca363267302434a754cdc1ac089447c
dibyarupa jena <dibyathecool.jena@gmail.com>
2020-08-20 11:09:19 +0530

    two screens, homescreen and productscreen for redirecting created with browserRouter, Route, link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import data from './data.js';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 function App() {
 
@@ -13,6 +16,7 @@ function App() {
   }
 
   return (
+  <BrowserRouter>  
     <div className="grid-container">
     <header className="header">
                                                   
@@ -20,7 +24,8 @@ function App() {
             <button onClick={openMenu}>
                 &#9776;                         
             </button>
-            <a href="index.html">Amazona</a>
+            <Link to="/">Vegmazon</Link>
+            
         </div>
         <div className="header-links">
             <a href="cart.html">Cart  &nbsp;&nbsp;&nbsp;&nbsp;</a>
@@ -42,30 +47,16 @@ function App() {
     </aside>
     <main className="main">
         <div className="content">
-            <ul className="products">
-                {
-                    data.products.map(product =>
-                        <li>
-                        <div className="product">
-                            <img className="product-image" src={product.image} alt="product"/>
-                            <div className="product-name">
-                    <a href="product.html">{product.name}</a>
-                                </div>
-                            <div className="product-brand">{product.brand}</div>
-                            <div className="product-price">Rs{product.price}</div>
-                            <div className="product-rating">{product.rating} stars ({product.numReviews} reviews)</div>
-                        </div>
-                    </li>)
-                }
-               
-                
-            </ul>
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+            
         </div>
     </main>
     <footer className="footer">
         All rights reserved.
     </footer>
 </div>
+</BrowserRouter>  
   );
 }
 
